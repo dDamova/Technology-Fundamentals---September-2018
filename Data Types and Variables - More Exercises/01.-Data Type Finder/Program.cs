3f1d17e59145da55693c723ddb7b07cde07f002c
desislavaDamova <43536216+desislavaDamova@users.noreply.github.com>
2019-01-03 20:55:39 +0200

    Create Program.cs

---
+++ Data Types and Variables - More Exercises/01.-Data Type Finder/Program.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace _01._DataTypeFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (int.TryParse(input, out int result1))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (double.TryParse(input, out double result2))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (char.TryParse(input, out char result3))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else if (bool.TryParse(input, out bool result4))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
