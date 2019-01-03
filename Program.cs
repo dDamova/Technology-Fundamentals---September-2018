8b9dfb141150f606b11d55252da8d9895489344a
desislavaDamova <43536216+desislavaDamova@users.noreply.github.com>
2019-01-03 14:54:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IntroAndBasicSyntax_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            byte age = byte.Parse(Console.ReadLine());
+            float grade = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:F2}");
+        }
+    }
+}
