9ac418c15e0d7522f42b17995411025d94e8ba88
desislavaDamova <43536216+desislavaDamova@users.noreply.github.com>
2019-01-03 19:06:58 +0200

    Create Program.cs

---
+++ Intro And Basic Syntax - Lab/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace _04._Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            minutes += 30;
+
+            if (minutes >= 60)
+            {
+                hours++;
+                minutes -= 60;
+            }
+            if (hours >= 24)
+            {
+                hours -= 24;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:D2}");
+        }
+    }
+}
