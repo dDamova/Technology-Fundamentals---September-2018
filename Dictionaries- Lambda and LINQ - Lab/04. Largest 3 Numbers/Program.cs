a0d5e6559fc5f40d7203da314e34aa3069dd38d3
desislavaDamova <43536216+desislavaDamova@users.noreply.github.com>
2019-01-04 14:51:44 +0200

    Create Program.cs

---
+++ Dictionaries- Lambda and LINQ - Lab/04. Largest 3 Numbers/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+
+namespace Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderByDescending(n => n)
+                .Take(3)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
