4f27f6fa4a4ad65de05d29f25a19544d47307ae5
desislavaDamova <43536216+desislavaDamova@users.noreply.github.com>
2019-01-04 16:16:46 +0200

    Create Program.cs

---
+++ Strings and Text Processing - Lab/05. Digits- Letters and Other/Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Text;
+
+namespace DigitsLettersAndOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder digits = new StringBuilder();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder others = new StringBuilder();
+
+            foreach (char symbol in input)
+            {
+                if (char.IsDigit(symbol))
+                {
+                    digits.Append(symbol);
+                }
+                else if (char.IsLetter(symbol))
+                {
+                    letters.Append(symbol);
+                }
+                else
+                {
+                    others.Append(symbol);
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
